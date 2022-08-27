c96e875345b1bc34b3d5c530cd0e4789e14be7df
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-27 13:28:21 -0400

    Make main.js file

--- index.js
+++ index.js
@@ -54,3 +54,30 @@ fs.writeFileSync(dirnameReal + "/html-builder-cli-temp/package.json", JSON.strin
         }
     }
 }));
+
+fs.writeFileSync(dirnameReal + "/html-builder-cli-temp/main.js", `
+    const { app, BrowserWindow, nativeTheme } = require("electron");
+
+    function createWindow () {
+
+        const mainWindow = new BrowserWindow({
+            width: 1200,
+            height: 900,
+            webPreferences: {
+                nodeIntegration: true,
+            }
+        });
+
+        mainWindow.setMenuBarVisibility(false);
+        mainWindow.loadFile("app/index.html");
+        mainWindow.maximize();
+        nativeTheme.themeSource = "${manifestData.colorScheme || "system"}";
+
+    }
+
+    app.whenReady().then(() => {
+        createWindow();
+    });
+
+    app.on("window-all-closed", function () { app.quit(); });
+`);
