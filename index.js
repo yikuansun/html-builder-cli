561e013d2400e9efa6bb8826eba300d56b0d2bd5
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-27 13:10:19 -0400

    Fix dirname

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const admZip = require("adm-zip");
 const fs = require("fs");
 const { exec } = require("child_process");
 
+var dirnameReal = process.cwd();
+
 var zip = new admZip();
-zip.addLocalFolder(__dirname);
+zip.addLocalFolder(dirnameReal);
 
-fs.mkdirSync(__dirname + "/html-builder-cli-temp");
-fs.mkdirSync(__dirname + "/html-builder-cli-temp/html");
+fs.mkdirSync(dirnameReal + "/html-builder-cli-temp");
+fs.mkdirSync(dirnameReal + "/html-builder-cli-temp/html");
 
-zip.extractAllTo(__dirname + "/html-builder-cli-temp/html");
+zip.extractAllTo(dirnameReal + "/html-builder-cli-temp/html");
