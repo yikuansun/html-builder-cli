fe14ebf693d42861a9884c21d0763dc504d9c621
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-27 13:12:24 -0400

    Make a package.json file

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ fs.mkdirSync(dirnameReal + "/html-builder-cli-temp");
 fs.mkdirSync(dirnameReal + "/html-builder-cli-temp/html");
 
 zip.extractAllTo(dirnameReal + "/html-builder-cli-temp/html");
+
+fs.writeFileSync(dirnameReal + "/html-builder-cli-temp/package.json", JSON.stringify({
+    name: "my-html-app" + Math.random().toString().substring(2, 6),
+    productName: "{app_name}",
+    description: "An app created with HTML Builder",
+    author: {
+        name: "HTML Builder (CLI)",
+        url: "https://github.com/yikuansun/html-builder-cli"
+    },
+    version: "1.0.0",
+    main: "main.js",
+    scripts: {
+        start: "electron .",
+        "build-linux": "electron-builder --linux",
+        "build-darwin": "electron-builder --mac",
+        "build-win32": "electron-builder --windows"
+    },
+    devDependencies: {
+        electron: "^17.1.0",
+        "electron-builder": "^22.14.5"
+    },
+    build: {
+        appId: "com.electron.htmlapp" + Math.random().toString().substring(2, 6),
+        directories: {
+            buildResources: "htmlbuilder-buildresources"
+        },
+        mac: {
+            "target": "zip"
+        }
+    }
+}));
