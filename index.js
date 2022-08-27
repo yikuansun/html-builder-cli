6bc8245c27e27e9a03823bd4a376d2e1939c9189
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-27 11:59:07 -0400

    Create directories

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const admZip = require("adm-zip");
+const fs = require("fs");
+const { exec } = require("child_process");
+
+var zip = new admZip();
+zip.addLocalFolder(__dirname);
+
+fs.mkdirSync(__dirname + "/html-builder-cli-temp");
+fs.mkdirSync(__dirname + "/html-builder-cli-temp/html");
+
+zip.extractAllTo(__dirname + "/html-builder-cli-temp/html");
